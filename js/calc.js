d70cb1fc2fe6dc9cc18e921e324efe35c656d8f0
Ray-Shakex <bshakex@gmail.com>
2025-10-09 09:36:37 +0200

    js: calculate function

--- js/calc.js
+++ js/calc.js
@@ -1,52 +1,26 @@
-// INPUT VARIABLES
 
-// OUTPUT VARIABLES 
-// function myFunction() {
-//     const price = document.getElementById("price").value;
-//     const amount = parseInt(price);
-
-//     const deposit = document.getElementById("deposit").value;
-//     const depositAmount = parseInt(deposit);
-//     console.log(deposit);
-
-
-//     const balance = price - deposit;
-//     console.log(balance);
-
-//     emp.innerHTML = price;
-//     console.log(emp);
-//     finance.innerHTML = balance;
-//     console.log(finance);
-// } 
 
 function Calculate() {
-
-    // Extracting value in the amount 
+    // INPUT VARIABLES
+    // Extracting value in the PRICE AMOUNT 
     // section in the variable
     const amount = document.querySelector("#price").value;
-
-    // Extracting value in the interest
-    // rate section in the variable
+    // Extracting value in the INTEREST RATE
     const rate = document.querySelector("#interest").value;
-
-    // Extracting value in the months 
-    // section in the variable
+    // Extracting value in the MONTHS/TERM 
     const months = document.querySelector("#months").value;
-
+    // Extracting value in the DEPOSIT 
+    const deposit = document.getElementById("deposit").value;
+    // REMAINING FINANCE 
+    const finance = document.getElementById("finance");
     // Calculating interest per month
     const interest = (amount * (rate * 0.01)) / months;
-    
     // Calculating total payment
     const total = ((amount / months) + interest).toFixed(2);
-
+    // INSERT TOTAL IN EMP ELEMENT
     document.querySelector("#emp")
             .innerHTML = total;
-
-    const deposit = document.getElementById("deposit").value;
-    const finance = document.getElementById("finance");
-    console.log(deposit);
-    console.log(finance);
-    // const balance = price - deposit;
+    // INSERT REMAINING FINANCE INTO ELEMENT
     finance.innerHTML = amount - deposit;
     
 }
