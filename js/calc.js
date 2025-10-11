3cea0f401455aabb7292a59986c0e23539c07d18
Ray-Shakex <bshakex@gmail.com>
2025-10-11 11:08:47 +0200

    js: balloon logic

--- js/calc.js
+++ js/calc.js
@@ -4,23 +4,28 @@ function Calculate() {
     // INPUT VARIABLES
     // Extracting value in the PRICE AMOUNT 
     // section in the variable
-    const amount = document.querySelector("#price").value;
-    // Extracting value in the INTEREST RATE
-    const rate = document.querySelector("#interest").value;
-    // Extracting value in the MONTHS/TERM 
-    const months = document.querySelector("#months").value;
+    const amount = parseFloat(document.querySelector("#price").value);
     // Extracting value in the DEPOSIT 
-    const deposit = document.getElementById("deposit").value;
+    const deposit = parseFloat(document.getElementById("deposit").value);
+    // Extracting value in the INTEREST RATE
+    const rate = parseFloat(document.querySelector("#interest").value) /100;
+    // Extracting value in the MONTHS/TERM 
+    const months = parseInt(document.querySelector("#months").value);
+    // Extracting value in the BALLOON 
+    const balloon = parseFloat(document.getElementById("balloon").value) / 100;
     // REMAINING FINANCE 
-    const finance = document.getElementById("finance");
+    // const finance = document.getElementById("finance");
     // Calculating interest per month
     const interest = (amount * (rate * 0.01)) / months;
+    // Calculating BALLOON 
+    const balloonAmount = amount * balloon;
     // Calculating total payment
     const total = ((amount / months) + interest).toFixed(2);
     // INSERT TOTAL IN EMP ELEMENT
     document.querySelector("#emp")
             .innerHTML = total;
     // INSERT REMAINING FINANCE INTO ELEMENT
-    finance.innerHTML = amount - deposit;
-    
+    document.querySelector("#finance").innerHTML = 'R' + (amount - deposit);
+    // INSERT BALLOON AMOUNT INTO ELEMENT
+    document.querySelector("#balloonAmount").innerHTML = 'R' + balloonAmount;
 }
